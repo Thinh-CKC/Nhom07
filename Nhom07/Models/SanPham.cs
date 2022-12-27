9df0c5eaaab1ea71afd02a7f9ecd51a838ecf444
PhuThinh <0306201291@caothang.edu.vn>
2022-12-27 14:51:56 +0700

    Upload model taikhoan va sanpham

---
+++ Nhom07/Models/SanPham.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nhom07.Models
+{
+    public class SanPham
+    {
+        public int ID { get; set; }
+
+        [DisplayName("Tên sản phẩm")]
+        [Required(ErrorMessage = "{0} không được bỏ trống")]
+        public string TenSP { get; set; }
+
+        [DisplayName("Loại sản phẩm")]
+        public int ID_LoaiSP { get; set; }
+
+        // Reference navigation property cho khóa ngoại đến LoaiSanPham 
+        [DisplayName("Loại sản phẩm")]
+        public LoaiSanPham LoaiSanPham { get; set; }
+
+        [DisplayName("Size")]
+        public string Size { get; set; }
+
+        [DisplayName("Màu sắc")]
+        public string MauSac { get; set; }
+
+        [DisplayName("Tồn kho")]
+        public int TonKho { get; set; }
+
+        [DisplayName("Mặt kính")]
+        public string MatKinh { get; set; }
+
+        [DisplayName("Loại dây")]
+        public string LoaiDay { get; set; }
+
+        [DisplayName("Hình ảnh")]
+        public string Anh { get; set; }
+
+        [DisplayName("Giá")]
+        [DisplayFormat(DataFormatString = "{0:n0}")]
+        public int Gia { get; set; }
+
+        [DisplayName("Nổi bật")]
+        public bool NoiBat { get; set; }
+
+        // Collection navigation property cho khóa ngoại từ GioHang
+        public List<GioHang> GioHangs { get; set; }
+
+        // Collection navigation property cho khóa ngoại từ ChiTietHoaDon
+        public List<ChiTietHoaDon> ChiTietHoaDons { get; set; }
+
+        // Collection navigation property cho khóa ngoại từ ChiTietSanPham
+        public List<HinhAnh> HinhAnhs { get; set; }
+    }
+}
---
+++ Nhom07/Models/TaiKhoan.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nhom07.Models
+{
+    public class TaiKhoan
+    {
+        public int ID { get; set; }
+
+        [DisplayName("Họ và tên")]
+        public string HoTen { get; set; }
+
+        [MaxLength(150)]
+        [Required(ErrorMessage = "Vui lòng nhập Email")]
+        [DataType(DataType.EmailAddress)]
+
+        public string Email { get; set; }
+
+        [MaxLength(11)]
+        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
+        [Display(Name = "Điện thoại")]
+        [DataType(DataType.PhoneNumber)]
+        public string SDT { get; set; }
+
+        [DisplayName("Địa chỉ")]
+        public string DiaChi { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [MinLength(5, ErrorMessage = "Bạn cần đặt mật khẩu tối thiểu 5 ký tự")]
+        public string Password { get; set; }
+
+        [DisplayName("Admin")]
+        public bool IsAdmin { get; set; }
+
+        // Collection navigation property cho khóa ngoại từ HoaDon
+        public List<HoaDon> HoaDons { get; set; }
+
+        // Collection navigation property cho khóa ngoại từ GioHang
+        public List<GioHang> GioHangs { get; set; }
+    }
+}
